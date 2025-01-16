44ac9b3cb364b67c2146eae6e4c96aabeb5c6262
Tam Love <studio@tamlove.com>
2025-01-16 11:24:10 +0000

    Initial commit / created project.

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import pluginVue from 'eslint-plugin-vue'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import pluginCypress from 'eslint-plugin-cypress/flat'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+
+  ...pluginVue.configs['flat/essential'],
+  ...vueTsEslintConfig(),
+  
+  {
+    ...pluginCypress.configs.recommended,
+    files: [
+      ''**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',',
+      'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
+      'cypress/support/**/*.{js,ts,jsx,tsx}'
+    ],
+  },
+]
